d6edf3e1edd385d36d72d1b678e2c50bb4c43cdb
Kanchan Kaushal <60342161+KanchanKaushal238@users.noreply.github.com>
2024-02-07 19:32:01 +0530

    Added solution of Day15_ValidParentheses_Problem_20.cs

--- Day15_ValidParentheses_Problem_20.cs
+++ Day15_ValidParentheses_Problem_20.cs
@@ -1,1 +1,103 @@
 
+/*
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: s = "()"
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * Example 3:
+ * 
+ * Input: s = "(]"
+ * Output: false
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'. 
+*/
+public class Solution
+{
+    public static bool IsValid(string s)
+    {
+        char[] sChar = s.ToCharArray();
+
+        Stack stackChar = new Stack();
+
+        // eliminates first digit as closed brackets 
+        if(sChar[0] == '}' || sChar[0] == ')' || sChar[0] == ']')
+        {
+            return false;
+        }
+
+        // eliminates 1 digit count
+        if(string.IsNullOrWhiteSpace(s) || sChar.Length == 0 || sChar.Length == 1)
+        {
+            return false;
+        }
+
+        // eleminate odd number count
+        if (sChar.Length % 2 != 0) return false;
+
+        int count = 0;
+
+        for(int i = 0; i< sChar.Length; i++)
+        {
+            if (sChar[i] == '{' || sChar[i] == '(' || sChar[i] == '[' )
+            {
+                count++;
+                stackChar.Push(sChar[i]);
+            }
+            if (stackChar.Count != 0 && (sChar[i] == '}' || sChar[i] == ')' || sChar[i] == ']'))
+            {
+                if (Convert.ToChar(stackChar.Peek()) == '(' && sChar[i] == ')')
+                {
+                    count++;
+                    stackChar.Pop();
+                }
+                else if (Convert.ToChar(stackChar.Peek()) == '{' && sChar[i] == '}')
+                {
+                    count++;
+                    stackChar.Pop();
+                }
+                else if (Convert.ToChar(stackChar.Peek()) == '[' && sChar[i] == ']')
+                {
+                    count++;
+                    stackChar.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if (count != sChar.Length) return false;
+        if (stackChar == null || stackChar.Count != 0) return false;
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        bool isValidBrackets = IsValid(s);
+
+        Console.WriteLine(isValidBrackets);
+    }
+}
