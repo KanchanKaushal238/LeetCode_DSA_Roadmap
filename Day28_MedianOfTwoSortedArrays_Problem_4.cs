58cb266be01d10d04c460748fda5149872ef0694
Kanchan Kaushal <60342161+KanchanKaushal238@users.noreply.github.com>
2024-02-26 22:54:08 +0530

    Added solution with optimized time complexity Day28_MedianOfTwoSortedArrays_Problem_4

--- Day28_MedianOfTwoSortedArrays_Problem_4.cs
+++ Day28_MedianOfTwoSortedArrays_Problem_4.cs
@@ -29,7 +29,7 @@
  * -106 <= nums1[i], nums2[i] <= 106
 */
 
-***************************Time Complexity => n(1+logn)******************************************************************
+***************************Time Complexity => O(n(1+logn))******************************************************************
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
         List<int> mergedList = new List<int>();
@@ -58,5 +58,61 @@ public class Solution {
 
             return (med1 + med2) / 2;
         }
+    }
+}
+***************************Time Complexity => O(log(m+n))******************************************************************
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] temp;
+        // swapping for traversing min array
+        if(nums1.Length > nums2.Length)
+        {
+            temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int n = nums1.Length, m = nums2.Length;
+        int low = 0, high = n;
+        while(low <= high)
+        {
+            // partitionX is half of X
+            int partitionX = (low + high) / 2;
+            // partitionY is half of total merged ele and minus partitionX
+            int partitionY = (n + m + 1) / 2 - partitionX;
+
+            // maxX is 1 less tan last partitionX ele
+            int maxX = partitionX == 0 ? int.MinValue : nums1[partitionX - 1];
+            // maxX is 1 less tan last partitionY ele
+            int maxY = partitionY == 0 ? int.MinValue : nums2[partitionY - 1];
+            
+            // minX is last partitionX ele
+            int minX = partitionX == n ? int.MaxValue : nums1[partitionX];
+            // minY is last partitionY ele
+            int minY = partitionY == m ? int.MaxValue : nums2[partitionY];
+
+            partitionX ele >= partitionY-1 ele && [artition 
+            if(minY >= maxX && minX >= maxY)
+            {
+                if((m+n)%2 == 0)
+                {
+                    return (double)(Math.Max(maxX, maxY) + Math.Min(minX, minY)) / 2;
+                }
+                else
+                {
+                    return (double)Math.Max(maxX, maxY);
+                }
+            }
+            else if(maxX > minY)
+            {
+                high = partitionX - 1;
+            }
+            else
+            {
+                low = partitionX + 1;
+            }
+        }
+
+        return (double)0.0;
     }
 }
